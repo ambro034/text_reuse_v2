c1596a584eb92b17a808243bf17c4b4465edfcb9
Graham Ambrose <123208997+ambro034@users.noreply.github.com>
2025-01-02 14:26:02 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+  name = 'textreuse',
+  version = '0.0.1',
+  description = "A package used to identify reused test between two portions of text",
+  url = 'https://github.com/ambro034/text_reuse.git',
+  author_name = 'Graham W. Ambrose',
+  license = 'unlicense',
+  package = ['textreuse2']
+  
+)
